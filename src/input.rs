e3437dd070f6f507341f87d430848cd70849e223
FreezyLemon <contact@freezylemon.com>
2023-12-02 17:54:12 +0100

    Edit input.rs to work with the new dayAB-partX format

--- src/input.rs
+++ src/input.rs
@@ -4,13 +4,15 @@ pub fn get_input() -> Result<String, Box<dyn std::error::Error>> {
     // skip first arg (binary name)
     let mut args = env::args();
     let bin_name = args.next().unwrap();
-    let path = PathBuf::from_str(&bin_name)?;
+    let bin_file = PathBuf::from_str(&bin_name)?;
+    let bin_file_name = bin_file.file_name().unwrap().to_string_lossy();
+
+    let (day, _part) = bin_file_name.split_once('-').expect("binary name contains '-'");
 
     let input_path = if let Some(arg) = args.next() {
         arg
     } else {
-        // convenience
-        format!("inputs/{}.txt", path.file_name().unwrap().to_string_lossy())
+        format!("inputs/{}.txt", day) // convenience
     };
 
     Ok(std::fs::read_to_string(input_path)?)
