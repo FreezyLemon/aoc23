558536de900523365af27c07b7b310c364bb3f7a
FreezyLemon <contact@freezylemon.com>
2023-12-09 21:36:06 +0100

    WIP: Day 5, Part 2

--- src/days/day05_part2.rs
+++ src/days/day05_part2.rs
@@ -1,3 +1,5 @@
+use std::cmp::{max, min};
+
 pub struct Day5Part2;
 
 impl crate::days::Day for Day5Part2 {
@@ -6,16 +8,14 @@ impl crate::days::Day for Day5Part2 {
 
         let (_, seed_range_str) = chunks_iter.next().expect("has content").split_once(':').expect("has :");
 
-        let mut ints = seed_range_str.trim().split_ascii_whitespace();
-        let mut seeds = vec![];
-        while let Some(range_start) = ints.next() {
-            let len: i64 = ints.next().expect("seeds come in pairs/ranges").parse().unwrap();
-            let range_start = range_start.parse().unwrap();
-
-            for s in range_start..range_start+len {
-                seeds.push(s);
-            }
-        }
+        let ints: Vec<i64> = seed_range_str.trim()
+            .split_ascii_whitespace()
+            .map(str::parse)
+            .map(Result::unwrap)
+            .collect();
+        let mut seed_ranges: Vec<_> = ints.chunks_exact(2)
+            .map(|c| (c[0]..c[0] + c[1], 0))
+            .collect();
 
         let map_categories = chunks_iter.map(|c| c.lines().skip(1))
             .map(|lines| {
@@ -26,22 +26,65 @@ impl crate::days::Day for Day5Part2 {
                         let src_start = ints.next().unwrap();
                         let len = ints.next().unwrap();
 
-                        (src_start..src_start + len, dst_start-src_start)
+                        (src_start..=src_start - 1 + len, dst_start-src_start)
                      })
                      .collect::<Vec<_>>()
             });
 
-        for maps in map_categories {
-            'seed: for s in &mut seeds {
-                for (range, offset) in &maps {
-                    if range.contains(s) {
-                        *s += offset;
-                        continue 'seed;
+        for category in map_categories {
+            for (map_range, offset) in category {
+                // println!("applying ({map_range:?}, {offset}) to {seed_ranges:?}");
+
+                let map_s = *map_range.start();
+                let map_e = *map_range.end() + 1;
+
+                let mut next_ranges = Vec::new();
+                for (seed_range, val) in seed_ranges {
+                    let seed_s = seed_range.start;
+                    let seed_e = seed_range.end;
+
+                    // check for intersection
+                    let lower_bound = max(map_s, seed_s);
+                    let upper_bound = min(map_e, seed_e);
+
+                    if lower_bound < upper_bound {
+                        next_ranges.push((lower_bound..upper_bound, val + offset));
+
+                        // leftovers
+                        if seed_s < lower_bound {
+                            next_ranges.push((seed_s..lower_bound, val));
+                        }
+
+                        if upper_bound < seed_e {
+                            next_ranges.push((upper_bound..seed_e, val));
+                        }
+                    } else {
+                        next_ranges.push((seed_s..seed_e, val));
                     }
                 }
+
+                seed_ranges = next_ranges;
+                // println!("resulting in {seed_ranges:?}");
+                // println!();
             }
+
+            // println!();
         }
 
-        seeds.into_iter().min().expect("has at least one value").to_string()
+        // println!("{seed_ranges:?}");
+
+        let final_vals: Vec<i64> = seed_ranges.into_iter()
+            .map(|(range, offset)| range.start + offset)
+            .collect();
+
+        println!("{final_vals:?}");
+
+        final_vals.into_iter()
+            .min()
+            .unwrap()
+            .to_string()
     }
 }
+
+
+
