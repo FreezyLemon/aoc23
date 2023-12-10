e7f57a41f8f10a05afd70210b2686e16c9dab688
FreezyLemon <contact@freezylemon.com>
2023-12-10 12:30:11 +0100

    Revert "Slightly refactor day 4 part 2 for readability"
    This reverts commit 5530c3d2722c53b59a965f5c3b70129bda6994d5.

--- src/days/day04_part2.rs
+++ src/days/day04_part2.rs
@@ -20,13 +20,12 @@ impl crate::days::Day for Day4Part2 {
             })
             .enumerate()
             .filter(|(_, w)| *w > 0)
-            .fold(vec![1u32; card_count], |amnts: Vec<u32>, (idx, win_amount)| {
+            .fold(vec![1u32; card_count], |mut amnts: Vec<u32>, (idx, win_amount)| {
                 let curr_amount = amnts[idx];
-                amnts.iter()
-                    .skip(idx + 1)
-                    .take(win_amount as usize)
-                    .map(|&amnt| amnt + curr_amount)
-                    .collect()
+                for i in idx+1..=(idx + win_amount as usize) {
+                    amnts[i] += curr_amount;
+                }
+                amnts
             })
             .into_iter()
             .sum::<u32>()
