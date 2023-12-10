f4e2fb97da9e5fb47f0617fe288454f28867fec4
FreezyLemon <contact@freezylemon.com>
2023-12-10 19:01:54 +0100

    Add benches for Day 9

--- benches/benches.rs
+++ benches/benches.rs
@@ -5,7 +5,7 @@ use aoc23::*;
 fn bench_day1_part2(c: &mut Criterion) {
     c.bench_function("day 1 part 2", |b| {
         let day = Day1Part2;
-        let input = get_input("day01-part2", None).unwrap();
+        let input = get_input("d1p2", None).unwrap();
 
         b.iter(|| day.solve(black_box(input.clone())));
     });
@@ -14,7 +14,7 @@ fn bench_day1_part2(c: &mut Criterion) {
 fn bench_day1_part2_malox(c: &mut Criterion) {
     c.bench_function("day 1 part 2 malox", |b| {
         let day = Day1Part2Malox;
-        let input = get_input("day01-part2", None).unwrap();
+        let input = get_input("d1p2", None).unwrap();
 
         b.iter(|| day.solve(black_box(input.clone())));
     });
@@ -23,7 +23,25 @@ fn bench_day1_part2_malox(c: &mut Criterion) {
 fn bench_day4_part2(c: &mut Criterion) {
     c.bench_function("day 4 part 2", |b| {
         let day = Day4Part2;
-        let input = get_input("day04-part2", None).unwrap();
+        let input = get_input("d4p2", None).unwrap();
+
+        b.iter(|| day.solve(black_box(input.clone())));
+    });
+}
+
+fn bench_day9_part1(c: &mut Criterion) {
+    c.bench_function("day 9 part 1", |b| {
+        let day = Day9Part1;
+        let input = get_input("d9p1", None).unwrap();
+
+        b.iter(|| day.solve(black_box(input.clone())));
+    });
+}
+
+fn bench_day9_part2(c: &mut Criterion) {
+    c.bench_function("day 9 part 2", |b| {
+        let day = Day9Part2;
+        let input = get_input("d9p2", None).unwrap();
 
         b.iter(|| day.solve(black_box(input.clone())));
     });
@@ -34,6 +52,8 @@ criterion_group!(
     bench_day1_part2,
     bench_day1_part2_malox,
     bench_day4_part2,
+    bench_day9_part1,
+    bench_day9_part2,
 );
 
 criterion_main!(benches);
