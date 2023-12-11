48eac0a582349ec327d89f523745265ca31baf48
FreezyLemon <contact@freezylemon.com>
2023-12-11 20:19:52 +0100

    Start Day 5, Part 2 rewrite

--- src/days/day05_part2.rs
+++ src/days/day05_part2.rs
@@ -1,4 +1,4 @@
-use std::cmp::{max, min};
+use std::{cmp::{max, min}, ops::RangeInclusive};
 
 pub struct Day5Part2;
 
@@ -14,7 +14,7 @@ impl crate::days::Day for Day5Part2 {
             .map(Result::unwrap)
             .collect();
         let mut seed_ranges: Vec<_> = ints.chunks_exact(2)
-            .map(|c| (c[0]..c[0] + c[1], 0))
+            .map(|c| (c[0]..=c[0] - 1 + c[1], 0))
             .collect();
 
         let map_categories = chunks_iter.map(|c| c.lines().skip(1))
@@ -26,47 +26,33 @@ impl crate::days::Day for Day5Part2 {
                         let src_start = ints.next().unwrap();
                         let len = ints.next().unwrap();
 
+                        debug_assert!(len > 0);
+
                         (src_start..=src_start - 1 + len, dst_start-src_start)
                      })
                      .collect::<Vec<_>>()
             });
 
         for category in map_categories {
+            let mut next_ranges = Vec::new();
+            println!("{category:?}");
+            println!();
             for (map_range, offset) in category {
-                let map_s = *map_range.start();
-                let map_e = *map_range.end() + 1;
-
-                let mut next_ranges = Vec::new();
+                let mut unmapped_ranges = Vec::new();
                 for (seed_range, val) in seed_ranges {
-                    let seed_s = seed_range.start;
-                    let seed_e = seed_range.end;
-
-                    // check for intersection
-                    let lower_bound = max(map_s, seed_s);
-                    let upper_bound = min(map_e, seed_e);
-
-                    if lower_bound < upper_bound {
-                        next_ranges.push((lower_bound..upper_bound, val + offset));
-
-                        // leftovers
-                        if seed_s < lower_bound {
-                            next_ranges.push((seed_s..lower_bound, val));
-                        }
-
-                        if upper_bound < seed_e {
-                            next_ranges.push((upper_bound..seed_e, val));
-                        }
-                    } else {
-                        next_ranges.push((seed_s..seed_e, val));
-                    }
+                    let (unmapped, mapped) = apply_map(seed_range, map_range.clone(), val, offset);
+                    next_ranges.extend(mapped);
+                    unmapped_ranges.extend(unmapped);
                 }
 
-                seed_ranges = next_ranges;
+                seed_ranges = unmapped_ranges;
             }
+
+            seed_ranges.extend(next_ranges);
         }
 
         let final_vals: Vec<i64> = seed_ranges.into_iter()
-            .map(|(range, offset)| range.start + offset)
+            .map(|(range, offset)| range.start() + offset)
             .collect();
 
         final_vals.into_iter()
@@ -76,5 +62,27 @@ impl crate::days::Day for Day5Part2 {
     }
 }
 
+fn apply_map(range: RangeInclusive<i64>, map: RangeInclusive<i64>, val: i64, map_offset: i64) -> (Vec<(RangeInclusive<i64>, i64)>, Vec<(RangeInclusive<i64>, i64)>) {
+    let mut unmapped = Vec::new();
+    let mut mapped = Vec::new();
 
+    let lower_bound = *max(range.start(), map.start());
+    let upper_bound = *min(range.end(), map.end());
+
+    if upper_bound >= lower_bound {
+        mapped.push((lower_bound..=upper_bound, val + map_offset));
+
+        if *range.start() < lower_bound {
+            unmapped.push((*range.start()..=lower_bound - 1, val));
+        }
+
+        if upper_bound < *range.end() {
+            unmapped.push((upper_bound + 1..=*range.end(), val));
+        }
+    } else {
+        unmapped.push((range, val));
+    }
+
+    (unmapped, mapped)
+}
 
