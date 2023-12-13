d9c6b306158dce64010126f0be29929b4feb291a
FreezyLemon <contact@freezylemon.com>
2023-12-13 03:20:23 +0100

    Optimize Day 6, Part 2

--- src/days/day06_part2.rs
+++ src/days/day06_part2.rs
@@ -8,22 +8,34 @@ impl crate::days::Day for Day6Part2 {
         let (_, distance) = distance.split_once(':').expect("has :");
 
         // assume all entries are valid ints
-        let time: i64 = time.split_ascii_whitespace()
+        let time: u64 = time.split_ascii_whitespace()
             .flat_map(str::chars)
             .collect::<String>()
             .parse()
             .expect("is valid int");
 
-        let record_dist: i64 = distance.split_ascii_whitespace()
+        let record: u64 = distance.split_ascii_whitespace()
             .flat_map(str::chars)
             .collect::<String>()
             .parse()
             .expect("is valid int");
-        
-        (1..time).zip((1..time).rev()) // possible accel/move combinations
-            .map(|(x, y)| x * y) // resulting distances
-            .filter(|d| *d > record_dist)
-            .count()
-            .to_string()
+
+        let mut upper_bound = time / 2;
+        let mut lower_bound = 1;
+        let mut idx = (upper_bound + lower_bound) / 2;
+        while upper_bound >= lower_bound {
+            idx = (upper_bound + lower_bound) / 2;
+            let next_idx = idx + 1;
+
+            if idx * (time - idx) > record {
+                upper_bound = idx - 1;
+            } else if next_idx * (time - next_idx) <= record {
+                lower_bound = idx + 1;
+            } else {
+                break;
+            }
+        }
+
+        (time - 1 - 2 * idx).to_string()
     }
 }
