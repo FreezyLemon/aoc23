897f49ff1c723df46e8e7835decbd02064798b1e
FreezyLemon <contact@freezylemon.com>
2023-12-13 02:53:18 +0100

    Optimize Day 6, Part 1

--- src/days/day06_part1.rs
+++ src/days/day06_part1.rs
@@ -8,16 +8,18 @@ impl crate::days::Day for Day6Part1 {
         let (_, distances) = distances.split_once(':').expect("has :");
 
         // assume all entries are valid ints
-        let times = times.split_ascii_whitespace().map(str::parse).map(Result::unwrap);
-        let records = distances.split_ascii_whitespace().map(str::parse).map(Result::unwrap);
+        let times = times.split_ascii_whitespace().map(str::parse::<u16>).map(Result::unwrap);
+        let records = distances.split_ascii_whitespace().map(str::parse::<u16>).map(Result::unwrap);
         
         times
             .zip(records)
             .map(|(t, r)| {
-                (1..t).zip((1..t).rev()) // possible accel/move combinations
-                    .map(|(x, y)| x * y) // resulting distances
-                    .filter(|d| *d > r)
-                    .count()
+                (1..t).zip((1..t).rev())
+                    .take(t as usize / 2) // avoid duplicate calculations if nothing found
+                    .enumerate()
+                    .find(|(_, (a, b))| a * b > r)
+                    .map_or(0, |(idx, _)| (t - 1) - 2 * idx as u16)
+                    as u32 // widen for .reduce()
             })
             .reduce(|acc, x| acc * x)
             .expect("is not empty")
