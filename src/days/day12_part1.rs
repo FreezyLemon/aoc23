dcb0f80250d81e476242360e2f92f98efd2f34b8
FreezyLemon <contact@freezylemon.com>
2023-12-17 16:02:04 +0100

    Revert "Improve Day 12, Part 1 slightly"
    It wasn't really an improvement. And the performance got worse too
    
    This reverts commit 7fb752fc5e68f88c42a73a5f5324f0502969e6e0.

--- src/days/day12_part1.rs
+++ src/days/day12_part1.rs
@@ -1,3 +1,5 @@
+use std::fmt::format;
+
 pub struct Day12Part1;
 
 impl crate::days::Day for Day12Part1 {
@@ -5,8 +7,6 @@ impl crate::days::Day for Day12Part1 {
         input.lines()
             .map(|line| {
                 let (pattern, arrangement) = line.split_once(' ').expect("input has whitespace");
-                let pattern: Vec<_> = pattern.trim_matches('.').chars().map(Spring::from_char).collect();
-                
                 let arrangement: Vec<i32> = arrangement.split(',')
                     .map(str::parse)
                     .collect::<Result<_, _>>()
@@ -14,33 +14,18 @@ impl crate::days::Day for Day12Part1 {
 
                 (pattern, arrangement)
             })
-            .map(|(pat, arrng)| munch(&pat, &arrng))
+            .map(|(pat, arrng)| {
+                let pat = pat.trim_matches('.');
+                munch(pat, &arrng)
+            })
             .sum::<i32>()
             .to_string()
     }
 }
 
-#[derive(PartialEq, Eq)]
-enum Spring {
-    Operational,
-    Unknown,
-    Damaged,
-}
-
-impl Spring {
-    fn from_char(c: char) -> Self {
-        match c {
-            '.' => Spring::Operational,
-            '?' => Spring::Unknown,
-            '#' => Spring::Damaged,
-            c => panic!("unsupported char {c}"),
-        }
-    }
-}
-
-fn munch(pattern: &[Spring], arrangements: &[i32]) -> i32 {
+fn munch(pattern: &str, arrangements: &[i32]) -> i32 {
     if arrangements.is_empty() {
-        if pattern.contains(&Spring::Damaged) {
+        if pattern.contains('#') {
             // there are still unused good springs,
             // so we can't finish the pattern here
             return 0;
@@ -56,7 +41,7 @@ fn munch(pattern: &[Spring], arrangements: &[i32]) -> i32 {
     }
 
     let next_arrng = arrangements[0] as usize;
-    let max_offset = match pattern.iter().enumerate().find(|(_, s)| **s == Spring::Damaged) {
+    let max_offset = match pattern.match_indices('#').nth(0) {
         Some((idx, _)) => std::cmp::min(max_offset, idx as i32),
         None => max_offset,
     };
@@ -65,9 +50,9 @@ fn munch(pattern: &[Spring], arrangements: &[i32]) -> i32 {
     for offset in 0..=max_offset as usize {
         let offset_pat = &pattern[offset..];
         if can_be_contiguous(&offset_pat[..next_arrng]) {
-            match offset_pat[next_arrng..].get(0) {
+            match offset_pat[next_arrng..].chars().nth(0) {
                 None if arrangements.len() == 1 => sum += 1, // end of pattern
-                Some(Spring::Damaged) => continue, // arrangements cannot be divided by a #
+                Some('#') => continue, // arrangements cannot be divided by a #
                 Some(_) => sum += munch(&offset_pat[1 + next_arrng..], &arrangements[1..]),
                 _ => {}
             }
@@ -77,6 +62,6 @@ fn munch(pattern: &[Spring], arrangements: &[i32]) -> i32 {
     sum
 }
 
-fn can_be_contiguous(s: &[Spring]) -> bool {
-    !s.contains(&Spring::Operational)
+fn can_be_contiguous(s: &str) -> bool {
+    !s.contains('.')
 }
