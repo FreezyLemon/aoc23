61cc47618ebe6c99e13daf38d8796187068475e5
FreezyLemon <contact@freezylemon.com>
2023-12-30 15:29:55 +0100

    WIP: Day 18, Part 2

--- src/main.rs
+++ src/main.rs
@@ -66,6 +66,7 @@ pub fn main() -> Result<(), Box<dyn std::error::Error>> {
         map_entry!(17, 1),
         map_entry!(17, 2),
         map_entry!(18, 1),
+        map_entry!(18, 2),
     ]);
 
     let mut args = std::env::args().skip(1);
