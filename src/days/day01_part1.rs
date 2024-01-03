cc7ee422abbe1a9fc12b4ed9fdc243f163247a7e
FreezyLemon <contact@freezylemon.com>
2024-01-03 01:17:25 +0100

    Refactor Day 1

--- src/days/day01_part1.rs
+++ src/days/day01_part1.rs
@@ -2,18 +2,17 @@ pub struct Day1Part1;
 
 impl crate::days::Day for Day1Part1 {
     fn solve(&self, input: &str) -> String {
-        let calibration_value: u32 = input
+        input
             .lines()
-            .map(|l| {
-                let mut all_digits = l.chars().filter_map(|c| c.to_digit(10));
+            .map(str::chars)
+            .map(|cs| cs.filter_map(|c| c.to_digit(10)))
+            .map(|mut all_digits| {
                 let first = all_digits.next().unwrap();
                 let last = all_digits.next_back().unwrap_or(first);
 
                 first * 10 + last
             })
-            .sum();
-
-        calibration_value.to_string()
+            .sum::<u32>()
+            .to_string()
     }
 }
-
--- src/days/day01_part2.rs
+++ src/days/day01_part2.rs
@@ -2,50 +2,41 @@ pub struct Day1Part2;
 
 impl crate::days::Day for Day1Part2 {
     fn solve(&self, input: &str) -> String {
-        let mut numbers = Vec::new();
-        for line in input.lines() {
-            let mut v = Vec::new();
-            
-            let mut i = 0;
-            while i < line.len() {
-                let l = &line[i..];
-                let first_char = l.chars().next().unwrap();
-                if let Some(d) = first_char.to_digit(10) {
-                    v.push(d);
-                    i += 1;
-                    continue;
-                }
-
-                if l.starts_with("one") {
-                    v.push(1);
-                } else if l.starts_with("two") {
-                    v.push(2);
-                } else if l.starts_with("three") {
-                    v.push(3);
-                } else if l.starts_with("four") {
-                    v.push(4);
-                } else if l.starts_with("five") {
-                    v.push(5);
-                } else if l.starts_with("six") {
-                    v.push(6);
-                } else if l.starts_with("seven") {
-                    v.push(7);
-                } else if l.starts_with("eight") {
-                    v.push(8);
-                } else if l.starts_with("nine") {
-                    v.push(9);
-                }
-
-                i += 1;
-            }
-
-            numbers.push(v);
-        }
-
-        let result: u32 = numbers.into_iter()
-            .map(|ds| *ds.first().unwrap() * 10 + *ds.last().unwrap())
-            .sum();
-
-        result.to_string()
+        input.lines()
+            .map(|line| (0..line.len()).map(|i| &line[i..]))
+            .map(|line_parts| {
+                line_parts.filter_map(|l| {
+                        let first_char = l.chars().next().unwrap();
+                        if let Some(d) = first_char.to_digit(10) {
+                            Some(d)
+                        } else if l.starts_with("one") {
+                            Some(1)
+                        } else if l.starts_with("two") {
+                            Some(2)
+                        } else if l.starts_with("three") {
+                            Some(3)
+                        } else if l.starts_with("four") {
+                            Some(4)
+                        } else if l.starts_with("five") {
+                            Some(5)
+                        } else if l.starts_with("six") {
+                            Some(6)
+                        } else if l.starts_with("seven") {
+                            Some(7)
+                        } else if l.starts_with("eight") {
+                            Some(8)
+                        } else if l.starts_with("nine") {
+                            Some(9)
+                        } else {
+                            None
+                        }
+                    })
+            })
+            .map(|mut ds| {
+                let first = ds.next().unwrap();
+                first * 10 + ds.next_back().unwrap_or(first)
+            })
+            .sum::<u32>()
+            .to_string()
     }
 }
